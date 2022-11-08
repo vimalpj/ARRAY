bf9cac9c6f2c0000359c4abc366d00d45f45b741
vimalpj <117161653+vimalpj@users.noreply.github.com>
2022-11-08 15:16:48 +0530

    Add files via upload

--- singeldaimention_array.java
+++ singeldaimention_array.java
@@ -1,10 +1,25 @@
 package ARRAY;
 
-public class singeldaimention_array {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class singeldaimention_array 
+{
+
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-
+		Scanner len =new Scanner(System.in);
+		System.out.println("ENTER THE LENTH");
+		         int lc=len.nextInt();
+                 int[] ab=new int[lc];
+                 int total=0;
+            	 System.out.println("ENTER THE VALUVE");
+            	 for(int i=0;i<ab.length;i++)
+            	 {
+            	 ab[i] =len.nextInt();
+            	 total=total+ ab[i];
+                 }
+            	System.out.println("total value="+total );
 	}
 
 }
